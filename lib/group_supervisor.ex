bd8f17dd8eff55d4c94e320ac04bb9cee667cd59
Dmitry Sinelnikov <dsinelnikov96@gmail.com>
2017-05-21 12:14:26 +0300

    Initial commit

---
+++ lib/group_supervisor.ex
@@ -0,0 +1,18 @@
+defmodule GroupSupervisor do
+  @moduledoc """
+  Documentation for GroupSupervisor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GroupSupervisor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/group_supervisor_test.exs
@@ -0,0 +1,8 @@
+defmodule GroupSupervisorTest do
+  use ExUnit.Case
+  doctest GroupSupervisor
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
